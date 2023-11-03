05ecdef2ab3282c8f51b77f37b1f91d22420a78c
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2023-11-03 13:09:22 +0100

    feat: add post model

---
+++ app/models/post.server.ts
@@ -0,0 +1,14 @@
+import { prisma } from "~/db.server";
+
+interface Post {
+  slug: string;
+  title: string;
+}
+
+export async function getPosts(): Promise<Post[]> {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
+}
