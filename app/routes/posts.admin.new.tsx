3051e0bfdf69cee43ed860244a2ecac915033164
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2023-11-03 13:35:23 +0100

    feat: add progressive enhancements

--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { createPost } from "~/models/post.server";
@@ -36,6 +36,9 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 export default function NewPost() {
   const errors = useActionData<typeof action>();
+
+  const navigation = useNavigation();
+  const isCreating = Boolean(navigation.state === "submitting");
 
   return (
     <Form method="post">
@@ -76,8 +79,9 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
