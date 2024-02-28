bd9362ae1d0f324f225b5870a2bdb7605427426e
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-02-28 23:17:31 +0100

    feat: :sparkles: add dummy store method

--- app/models/city.server.ts
+++ app/models/city.server.ts
@@ -1,3 +1,5 @@
+import { City } from "@prisma/client";
+
 import { prisma } from "~/db.server";
 
 export async function getCities(countyId: number | undefined = undefined) {
@@ -10,4 +12,8 @@ export async function getCities(countyId: number | undefined = undefined) {
 
 export async function getCity(id: number) {
   return prisma.city.findUnique({ where: { id } });
+}
+
+export async function store(city: City) {
+  return prisma.city.create({ data: city });
 }
